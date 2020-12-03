28c236ffda60f23b373a26f2a61e65c524c8c131
taitt <taitt@house3d.org>
2020-12-03 09:34:15 +0700

    push

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,6 +1,6 @@
 <p>Http client : {{$json_httpClient}}</p>
 <p>Bot: {{$json_bot}}</p>
 <p>Result: {{$json_result}}</p>
-<p>Http status: {{isset($httpStatus) ? '' : $httpStatus}}</p>
-<p>Body: {{isset($rawBody) ? '' : $rawBody}}</p>
-<p>End point : {{isset($endPoint) ? '' : $endPoint}}</p>
+<p>Http status: {{empty($httpStatus) ? '' : $httpStatus}}</p>
+<p>Body: {{empty($rawBody) ? '' : $rawBody}}</p>
+<p>End point : {{empty($endPoint) ? '' : $endPoint}}</p>
